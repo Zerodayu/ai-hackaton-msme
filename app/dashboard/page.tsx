9f350a9bdaadb1c6c6fc29ebc479090f6c98b076
Zerodayu <kimianalconera39@gmail.com>
2025-11-22 23:58:07 +0800

    update: table layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -29,13 +29,13 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6">
               <SectionCards />
-              <div className="grid grid-cols-2 gap-4 px-4 lg:px-6">
+              <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
-                <ChartBarMultiple />
               </div>
               {/* <DataTable data={data} /> */}
-              <div className="lg:flex-row gap-6 px-4 lg:px-6">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-4 lg:px-6">
                 <TableCard />
+                <ChartBarMultiple />
               </div>
             </div>
           </div>
